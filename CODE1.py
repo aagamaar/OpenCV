10b3dbf778abb86a236382a51aad85f300df4f6c
aagamaar <aagamaar01@gmail.com>
2025-01-18 19:05:04 +0530

    Added image and wrote some code

--- CODE1.py
+++ CODE1.py
@@ -1,3 +1,14 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+
+def read_file(filename):
+    img=cv2.imread(filename)
+    img=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    plt.imshow(img)
+    plt.show
+    return img
+
+filename= "images.jpg"
+read_file(filename)
+
